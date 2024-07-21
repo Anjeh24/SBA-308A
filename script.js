3daa59e98c49e26e7a4c244cfdef4fc108115740
anjeh24 <junez24@yahoo.com>
2024-07-21 18:39:43 -0400

    tried using cat API with demo key. Shows in console but no luck appending content to html

--- script.js
+++ script.js
@@ -6,16 +6,40 @@ let mainDiv = document.getElementById('content');
 clickBtn.addEventListener('click', theCatApi);
 
 
-async function theCatApi(){
-    const respns = await fetch('https://dog.ceo/api/breeds/image/random');
-    const jsdata = await respns.json();
-   const url = jsdata.message;
+// async function theCatApi(){
+//     const respns = await fetch(' https://api.thecatapi.com/v1/images/search');
+//     const jsdata = await respns.json();
+//    const url = jsdata.message;
 
-   console.log(respns);
-   console.log(url);
-   const images = document.createElement('img');
-   mainDiv.appendChild(images);
-   images.src = url;
+//    console.log(respns);
+//    console.log(url);
+//    const images = document.createElement('img');
+//   // mainDiv.appendChild(images);
+//    images.src = url;
    
+// }
+// theCatApi();
+
+//let try again!!!!!!!!!!!!!!!!!!!
+async function theCatApi(){
+const headers = new Headers({
+    "Content-Type": "application/json",
+    "x-api-key": "DEMO-API-KEY"
+  });
+  
+  var requestOptions = {
+    method: 'GET',
+    headers: headers,
+    redirect: 'follow'
+  };
+
+  fetch("https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1", requestOptions)
+    
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error)); 
+
+  //trying to insert content in html
+  futureContnt.innerHTML = result;;
+
 }
-theCatApi();

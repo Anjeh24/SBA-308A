69a2e4e80f9da27f1eb8f93770c0dde3d1681a47
anjeh24 <junez24@yahoo.com>
2024-07-21 22:21:42 -0400

    converted api images to blobs to have them attached. works but images not visible even after styling

---
+++ modules/module1.js
@@ -0,0 +1,3 @@
+ let my_Key = "live_bi2Lf3FLwbecVqlHqzjmusMuxToGnW4DuIxwlw1OvWdXqGvgOkLsfFu7UzKuHJQB";
+
+ export{my_Key};
--- script.js
+++ script.js
@@ -5,41 +5,20 @@ let mainDiv = document.getElementById('content');
 
 clickBtn.addEventListener('click', theCatApi);
 
-
-// async function theCatApi(){
-//     const respns = await fetch(' https://api.thecatapi.com/v1/images/search');
-//     const jsdata = await respns.json();
-//    const url = jsdata.message;
-
-//    console.log(respns);
-//    console.log(url);
-//    const images = document.createElement('img');
-//   // mainDiv.appendChild(images);
-//    images.src = url;
-   
-// }
-// theCatApi();
-
-//let try again!!!!!!!!!!!!!!!!!!!
 async function theCatApi(){
-const headers = new Headers({
-    "Content-Type": "application/json",
-    "x-api-key": "DEMO-API-KEY"
-  });
-  
-  var requestOptions = {
-    method: 'GET',
-    headers: headers,
-    redirect: 'follow'
-  };
+   const response = await fetch("https://api.thecatapi.com/v1/images/search");
+   const blobs = await response.blob();
+   console.log(blobs);
 
-  fetch("https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1", requestOptions)
+   document.getElementById('pets').src = URL.createObjectURL(blobs);
+   
     
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error)); 
+  }
+  theCatApi();
 
-  //trying to insert content in html
-  futureContnt.innerHTML = result;;
+//trying to import my key from module1.js
+//import { my_Key } from "./modules/module1";
 
-}
+
+
+
